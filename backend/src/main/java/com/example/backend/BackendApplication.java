3bf3ae1e1f47b611403a8a998d994018c13144bc
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2025-04-18 08:58:05 +0200

    refactor(back): simplify loading environmental variables

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.example.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,12 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-
         SpringApplication.run(BackendApplication.class, args);
     }
 
