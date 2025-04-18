f178195d1cd0d72c4334f35d18cf687ddfa8f0be
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2025-04-18 09:35:35 +0200

    feat(back/test): add 1st integration test for get queries route

---
+++ backend/src/test/java/com/example/backend/query/QueryControllerTest.java
@@ -0,0 +1,45 @@
+package com.example.backend.query;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+class QueryControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+//    @AfterEach
+//    void tearDown() {
+//    }
+
+    @Test
+    void getQueries_whenMockMvc_thenVerifyResponse() throws Exception {
+        this.mockMvc.perform(get("/api/queries"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$[?(@.name)]").isArray())
+                .andReturn();
+    }
+
+    @Test
+    void runQueries() {
+    }
+}
