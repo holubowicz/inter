6521687a8942b8a9ff90b776d3ad0ca2fc0fdf41
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2025-04-18 11:16:44 +0200

    feat(back/test): setup and use Testcontainers

--- backend/src/test/java/com/example/backend/query/QueryControllerTest.java
+++ backend/src/test/java/com/example/backend/query/QueryControllerTest.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -16,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@Testcontainers
 class QueryControllerTest {
 
     private MockMvc mockMvc;
@@ -23,14 +29,21 @@ class QueryControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:17-alpine")
+            .withInitScript("init.sql");
+
+    @DynamicPropertySource
+    static void setTestProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
     @BeforeEach
     public void setUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
-
-//    @AfterEach
-//    void tearDown() {
-//    }
 
     @Test
     void getQueries_whenMockMvc_thenVerifyResponse() throws Exception {
