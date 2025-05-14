58ee8ca26901f4927e66c64f4d1273f00a40d2aa
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2025-05-14 09:48:44 +0200

    feat(front): make header sticky to the top

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "@/components/theme/theme-toggle";
 
 export function Header() {
   return (
-    <header className="bg-secondary flex w-full justify-center border-b-2 py-3 md:py-4">
+    <header className="bg-secondary sticky top-0 flex w-full justify-center border-b-2 py-3 md:py-4">
       <div className="flex w-11/12 max-w-7xl items-center justify-between">
         <nav>
           <NavLink to="/" className="font-black uppercase md:text-xl">
